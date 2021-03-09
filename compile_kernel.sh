50794b21f04453cdcee0b0bb10b1c0b631a84af4
ArchivalLegion <antiads@protonmail.com>
2021-03-09 10:12:06 -0500

    Comment out flashing

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -2,7 +2,7 @@
 
 # Start by compiling and compressing the kernel + tidly bits
 echo "Starting the hellacious process"
-make Image
+make zImage
 make dtbs
 make modules
 make INSTALL_DTBS_PATH="/boot/dtbs" dtbs_install
@@ -16,9 +16,8 @@ echo "initramfs done!"
 
 # Generate the kernel image (vmlinux.uimg)
 echo "Generating kernel image"
-mkimage -C none -O linux -T kernel \
--b arch/boot/dts/rk3288-veyron-minnie.dtb \
--i /boot/initramfs-minnie.img \
+mkimage \
+-D "-I dts -O dtb -p 2048" \
 -f kernel.its vmlinux.uimg
 echo "Image done!"
 
@@ -41,12 +40,12 @@ vbutil_kernel \
 echo "depthcharge kernel done!"
 
 # Move files into /boot
-cp arch/arm/boot/Image /boot/Image
+cp arch/arm/boot/zImage /boot/zImage
 cp vmlinux.kpart /boot/vmlinux.kpart
 echo "Copied files into /boot"
 
 # Flash the image
-echo "Flashing the kernel image"
-dd if=/boot/vmlinux.kpart of=/dev/mmcblk1p1
-sync
-echo "Flashed!, everything is done!"
+#echo "Flashing the kernel image"
+#dd if=/boot/vmlinux.kpart of=/dev/mmcblk1p1
+#sync
+#echo "Flashed!, everything is done!"
